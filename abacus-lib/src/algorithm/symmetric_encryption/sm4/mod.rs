68dfe3836e321f00d5fe1af95af02b8d78d3891f
xiangxin <405340537@qq.com>
2022-03-14 15:01:48 +0800

    implement sm4 ofb and cfb.

--- abacus-lib/src/algorithm/symmetric_encryption/sm4/mod.rs
+++ abacus-lib/src/algorithm/symmetric_encryption/sm4/mod.rs
@@ -2,3 +2,5 @@ pub mod sm4_constant;
 pub mod sm4_encryption;
 pub mod sm4_ecb;
 pub mod sm4_cbc;
+pub mod sm4_ofb;
+pub mod sm4_cfb;
---
+++ abacus-lib/src/algorithm/symmetric_encryption/sm4/sm4_ofb.rs
@@ -0,0 +1,26 @@
+use super::sm4_constant::{KEY_BYTE_LENGTH, SM4_BLOCK_BYTE_LENGTH};
+use super::sm4_encryption::SM4Encryption;
+
+pub fn sm4_ofb_encrypt_data(origin_data: &[u8], key: [u8; KEY_BYTE_LENGTH], iv: [u8; SM4_BLOCK_BYTE_LENGTH]) -> Vec<u8> {
+    let sm4 = SM4Encryption::with_key(key);
+    let block_sum = origin_data.len() / SM4_BLOCK_BYTE_LENGTH;
+    let enciphered_data_length = block_sum * SM4_BLOCK_BYTE_LENGTH;
+    let mut key_with_iv = iv;
+    let mut key_with_iv_encrypted = [0; SM4_BLOCK_BYTE_LENGTH];
+    let mut enciphered_data: Vec<u8> = Vec::with_capacity(enciphered_data_length);
+    enciphered_data.resize(enciphered_data_length, 0);
+    for i in 0..block_sum {
+        sm4.encrypt(&key_with_iv, &mut key_with_iv_encrypted);
+        key_with_iv = key_with_iv_encrypted;
+        let origin_data_block: &[u8; SM4_BLOCK_BYTE_LENGTH] = (&origin_data[(i * SM4_BLOCK_BYTE_LENGTH)..((i + 1) * SM4_BLOCK_BYTE_LENGTH)]).try_into().unwrap();
+        let enciphered_data_block: &mut [u8; SM4_BLOCK_BYTE_LENGTH] = (&mut enciphered_data[(i * SM4_BLOCK_BYTE_LENGTH)..((i + 1) * SM4_BLOCK_BYTE_LENGTH)]).try_into().unwrap();
+        for j in 0..SM4_BLOCK_BYTE_LENGTH {
+            enciphered_data_block[j] = origin_data_block[j] ^ key_with_iv[j]
+        }
+    }
+    enciphered_data
+}
+
+pub fn sm4_ofb_decrypt_data(enciphered_data: &[u8], key: [u8; KEY_BYTE_LENGTH], iv: [u8; SM4_BLOCK_BYTE_LENGTH]) -> Vec<u8> {
+    sm4_ofb_encrypt_data(enciphered_data, key, iv)
+}
